469aace00eb385d5459db554fbb566b7de56e8f1
silvyanarisma <silvyana.risma@gmail.com>
2022-02-28 01:07:41 +0700

    create readme

--- app/src/main/java/com/silvy/MainActivity.kt
+++ app/src/main/java/com/silvy/MainActivity.kt
@@ -8,7 +8,7 @@ import com.silvy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
